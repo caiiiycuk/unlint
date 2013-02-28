cd04c0814bfe21ffc91b2daa1bc0ce97e1697821
Aleksander Guryanov <caiiiycuk@gmail.com>
2013-02-28 21:41:26 +0700

    fix chrome plugin

--- plugins/chrome/options.js
+++ plugins/chrome/options.js
@@ -1,7 +1,5 @@
 function onSave() {
-	localStorage['login'] = document.getElementById("login").value;
-	localStorage['password'] = document.getElementById("password").value;
-	localStorage['url'] = document.getElementById("url").value;
+	localStorage['token'] = document.getElementById("token").value;
 	window.close();
 }
 
@@ -11,6 +9,4 @@ function onClose() {
 
 document.getElementById("save").onclick = onSave;
 document.getElementById("cancel").onclick = onClose;
-document.getElementById("login").value = localStorage['login'] || "";
-document.getElementById("password").value = localStorage['password'] || "";
-document.getElementById("url").value = localStorage['url'] || "http://unlint.github.com";
+document.getElementById("token").value = localStorage['token'] || "";
