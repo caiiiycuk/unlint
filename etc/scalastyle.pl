a396335e713a08771d520276582e7e479026be7a
caiiiycuk <caiiiycuk@gmail.com>
2013-03-05 09:21:05 +0700

    Fix scala RPAREN problem

--- etc/scalastyle.pl
+++ etc/scalastyle.pl
@@ -6,7 +6,10 @@ my $file = $ARGV[1];
 my $xmlOutput = $file . ".out.xml";
 
 `scalastyle -q 1 -c $config --xmlOutput $xmlOutput  $file`;
-print `cat $xmlOutput`;
+my $out = `cat $xmlOutput`;
 `rm $xmlOutput`;
+
+$out =~ s|<error severity="error" message="Expected token RPAREN but got Token\(STRING_LITERAL.*?></error>||sg;
+print $out;
 
 0;
